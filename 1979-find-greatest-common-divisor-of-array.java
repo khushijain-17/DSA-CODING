5ae72370c8feba220a0b0acb6761f68644637004
Khushi Jain <khushijain.mail@gmail.com>
2026-09-14 21:21:00 +0530

    1979 Find Greatest Common Divisor of Array - Runtime: 0 ms, Memory: 44.7 MB

---
+++ 1979-find-greatest-common-divisor-of-array.java
@@ -0,0 +1,17 @@
+//euclidean algorithm 
+class Solution {
+    public int findGCD(int[] nums) {
+      int min = nums[0];
+      int max = nums[0];
+      for(int num : nums){
+        min = Math.min(min,num);
+        max = Math.max(max,num);
+      }
+      while(min != 0){
+        int rem = max % min;
+        max = min ;
+        min = rem;
+      }
+      return max;  
+    }
+}
